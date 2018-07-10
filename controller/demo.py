325c33872a1af9d659a14a18f6ffb8880825c031
teakilla <amakhanov3@gatech.edu>
2018-07-10 00:01:04 -0400

    Demo Account is not persistent

--- controller/demo.py
+++ controller/demo.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session, abort, request, jsonify, redirect
+from db import *
 
 app_demo = Blueprint('app_demo', __name__)
 
@@ -9,6 +10,42 @@ app_demo = Blueprint('app_demo', __name__)
 #############################################################################
 @app_demo.route("/demo", methods=['GET'])
 def admin():
-    session['username'] = "demo@demo"
-    session['admin'] = False
-    return redirect("/", code=302)
+    authors = ["Andrey Makhanov", "Ryan Lewis"]
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM user WHERE username = 'demo@demo';")
+    userrow = cur.fetchone()
+
+    myclasses = []
+    tclass = json.loads(userrow[5])
+
+    if userrow[4] == "Computing Systems":
+        smart_cnt = 4
+        # 6 courses
+    else:
+        smart_cnt = 5
+        # 5 courses
+
+    tclass.reverse()
+    for i in tclass:
+        cur.execute("SELECT course,course_name,rating,hours FROM classes WHERE course=?", [str(i)])
+        myclasses.append(cur.fetchone())
+
+    when = ["Fall 2018", "Spring 2019", "Fall 2019", "Spring 2020", "Fall 2020", "Spring 2021", "Fall 2021", "Spring 2022",
+            "Fall 2022", "Spring 2023"]
+    when_count = 0
+    myclasses2 = []
+    for i in myclasses:
+        zcnt = 0
+        if when_count < smart_cnt:
+            zcnt = 1
+        myclasses2.append([i[0], i[1], i[2], i[3], when[when_count], zcnt])
+        when_count += 1
+        # print(i+(4))
+
+    # cur.execute("SELECT * FROM user WHERE username = '" + session.get('username') + "';")
+    # userrow = cur.fetchone()
+    return render_template("index_output.html", authors=authors, hours=userrow[2], text=userrow[3][:300], spec=userrow[4],
+                           classes=myclasses2,
+                           personality=userrow[6:])
